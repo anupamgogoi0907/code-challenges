f4b17398fdf1ade6069443e3eaca78d93773c3ca
Anupam G <anupam@wso2.com>
2021-12-14 10:27:47 -0300

    Forgot to commit Controller tests

--- starwars-resistence-network/src/test/java/com/letscode/starwars/controller/RebeldeControllerTest.java
+++ starwars-resistence-network/src/test/java/com/letscode/starwars/controller/RebeldeControllerTest.java
@@ -1,9 +1,6 @@
 package com.letscode.starwars.controller;
 
-import com.letscode.starwars.dto.InventarioDTO;
-import com.letscode.starwars.dto.ItemDTO;
-import com.letscode.starwars.dto.LocalizacaoDTO;
-import com.letscode.starwars.dto.RebeldeDTO;
+import com.letscode.starwars.dto.*;
 import com.letscode.starwars.dto.response.AppResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -60,6 +57,81 @@ public class RebeldeControllerTest {
         LocalizacaoDTO localizacaoDTO = new LocalizacaoDTO().setNome("Galaxy2").setLatitude("100.0").setLongitude("200.0");
         ResponseEntity rp = rebeldeController.reportarLocal(id, localizacaoDTO);
         assertEquals(HttpStatus.ACCEPTED, rp.getStatusCode());
+    }
+
+    @Test
+    public void givenInputShouldReportTraidor() {
+        RebeldeDTO r1 = helper("A");
+        RebeldeDTO r2 = helper("B");
+        RebeldeDTO r3 = helper("C");
+        RebeldeDTO r4 = helper("D");
+        rebeldeController.createRebelde(r1);
+        rebeldeController.createRebelde(r2);
+        rebeldeController.createRebelde(r3);
+        rebeldeController.createRebelde(r4);
+
+        ReporterDTO rr1 = new ReporterDTO().setIdReporter(2).setIdTraidor(1);
+        ResponseEntity re1 = rebeldeController.reportarTraidor(rr1);
+        ReporterDTO rr2 = new ReporterDTO().setIdReporter(3).setIdTraidor(1);
+        ResponseEntity re2 = rebeldeController.reportarTraidor(rr2);
+        ReporterDTO rr3 = new ReporterDTO().setIdReporter(4).setIdTraidor(1);
+        ResponseEntity re3 = rebeldeController.reportarTraidor(rr3);
+        String appResponse = (String) ((AppResponse) re3.getBody()).getResponse();
+        assertEquals("Rebelde foi marcado como Traidor", appResponse);
+    }
+
+    @Test
+    public void givenInputShouldTrade() {
+        RebeldeDTO r1 = new RebeldeDTO()
+                .setNome("A")
+                .setTraidor(false)
+                .setGenero("M")
+                .setIdade(33)
+                .setLocalizacao(new LocalizacaoDTO().setNome("Galaxy1").setLatitude("1.0").setLongitude("1.0"))
+                .setInventario(new InventarioDTO().setItens(Arrays.asList(new ItemDTO().setNome("Arma").setQuantidade(1).setPontos(4))));
+
+        RebeldeDTO r2 = new RebeldeDTO()
+                .setNome("A")
+                .setTraidor(false)
+                .setGenero("M")
+                .setIdade(33)
+                .setLocalizacao(new LocalizacaoDTO().setNome("Galaxy1").setLatitude("1.0").setLongitude("1.0"))
+                .setInventario(new InventarioDTO().setItens(Arrays.asList(new ItemDTO().setNome("Água").setQuantidade(2).setPontos(4))));
+        rebeldeController.createRebelde(r1);
+        rebeldeController.createRebelde(r2);
+
+        TradeDTO r1Trade = new TradeDTO().setId(1).setItens(Arrays.asList(new ItemDTO().setNome("Arma").setQuantidade(1)));
+        TradeDTO r2Trade = new TradeDTO().setId(2).setItens(Arrays.asList(new ItemDTO().setNome("Água").setQuantidade(2)));
+
+        ResponseEntity responseEntity = rebeldeController.trade(new TradeDTO[]{r1Trade, r2Trade});
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+
+    }
+
+    @Test
+    public void givenInputShouldNotTrade(){
+        RebeldeDTO r1 = new RebeldeDTO()
+                .setNome("A")
+                .setTraidor(false)
+                .setGenero("M")
+                .setIdade(33)
+                .setLocalizacao(new LocalizacaoDTO().setNome("Galaxy1").setLatitude("1.0").setLongitude("1.0"))
+                .setInventario(new InventarioDTO().setItens(Arrays.asList(new ItemDTO().setNome("Arma").setQuantidade(1).setPontos(4))));
+
+        RebeldeDTO r2 = new RebeldeDTO()
+                .setNome("A")
+                .setTraidor(false)
+                .setGenero("M")
+                .setIdade(33)
+                .setLocalizacao(new LocalizacaoDTO().setNome("Galaxy1").setLatitude("1.0").setLongitude("1.0"))
+                .setInventario(new InventarioDTO().setItens(Arrays.asList(new ItemDTO().setNome("Água").setQuantidade(2).setPontos(4))));
+        rebeldeController.createRebelde(r1);
+        rebeldeController.createRebelde(r2);
+
+        TradeDTO r1Trade = new TradeDTO().setId(1).setItens(Arrays.asList(new ItemDTO().setNome("Arma").setQuantidade(1)));
+        TradeDTO r2Trade = new TradeDTO().setId(2).setItens(Arrays.asList(new ItemDTO().setNome("Água").setQuantidade(1)));
+        ResponseEntity responseEntity = rebeldeController.trade(new TradeDTO[]{r1Trade, r2Trade});
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
     }
 
     private RebeldeDTO helper(String nome) {
