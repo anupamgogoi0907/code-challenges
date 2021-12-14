133318bcac9f2dd29608738f9db40af7978a3735
Anupam G <anupam@wso2.com>
2021-12-14 13:00:17 -0300

    Forgot to commit Controller tests

--- starwars-resistence-network/src/test/java/com/letscode/starwars/mvc/APITest.java
+++ starwars-resistence-network/src/test/java/com/letscode/starwars/mvc/APITest.java
@@ -5,16 +5,21 @@ import com.letscode.starwars.dto.InventarioDTO;
 import com.letscode.starwars.dto.ItemDTO;
 import com.letscode.starwars.dto.LocalizacaoDTO;
 import com.letscode.starwars.dto.RebeldeDTO;
+import com.letscode.starwars.dto.response.AppResponse;
 import com.letscode.starwars.repository.RebeldeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.Arrays;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -36,6 +41,34 @@ public class APITest {
         mockMvc.perform(post("/rebelde").contentType("application/json")
                 .content(objectMapper.writeValueAsBytes(rebeldeDTO)))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void shouldReturnRebeldes() throws Exception {
+        RebeldeDTO rebeldeDTO = helper("Anupam");
+        mockMvc.perform(post("/rebelde").contentType("application/json")
+                .content(objectMapper.writeValueAsBytes(rebeldeDTO)))
+                .andExpect(status().isCreated());
+        mockMvc.perform(get("/rebelde"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.response").isNotEmpty());
+        System.out.println();
+    }
+
+    @Test
+    public void givenInputShouldCreateAndThenFindRebelde() throws Exception {
+        RebeldeDTO rebeldeDTO = helper("Anupam");
+        ResultActions resultActions = mockMvc.perform(post("/rebelde").contentType("application/json")
+                .content(objectMapper.writeValueAsBytes(rebeldeDTO)))
+                .andExpect(status().isCreated());
+        MvcResult result = resultActions.andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        AppResponse appResponse = objectMapper.readValue(contentAsString, AppResponse.class);
+        Integer id = (Integer) appResponse.getResponse();
+
+        // Check
+        mockMvc.perform(get("/rebelde/" + id)).andExpect(status().isFound());
+        System.out.println();
     }
 
     private RebeldeDTO helper(String nome) {
