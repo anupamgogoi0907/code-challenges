4df149d8e45838a628e77f7913e570ffafa093c2
Anupam G <anupam@wso2.com>
2021-12-14 10:33:23 -0300

    Forgot to commit Controller tests

--- starwars-resistence-network/src/main/java/com/letscode/starwars/controller/RelatorioController.java
+++ starwars-resistence-network/src/main/java/com/letscode/starwars/controller/RelatorioController.java
@@ -2,10 +2,12 @@ package com.letscode.starwars.controller;
 
 
 import com.letscode.starwars.dto.RelatorioDTO;
+import com.letscode.starwars.dto.response.AppResponse;
 import com.letscode.starwars.service.RelatorioService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,8 @@ public class RelatorioController {
     public ResponseEntity getReport() {
         logger.info("Executing " + this.getClass().getName() + ".getReport()");
         RelatorioDTO relatorioDTO = relatorioService.getReport();
-        return ResponseEntity.ok(relatorioDTO);
+        AppResponse appResponse = new AppResponse();
+        appResponse.setResponse(appResponse);
+        return new ResponseEntity(appResponse, HttpStatus.OK);
     }
 }
---
+++ starwars-resistence-network/src/test/java/com/letscode/starwars/controller/RelatorioControllerTest.java
@@ -0,0 +1,48 @@
+package com.letscode.starwars.controller;
+
+import com.letscode.starwars.dto.InventarioDTO;
+import com.letscode.starwars.dto.ItemDTO;
+import com.letscode.starwars.dto.LocalizacaoDTO;
+import com.letscode.starwars.dto.RebeldeDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class RelatorioControllerTest {
+    @Autowired
+    RelatorioController relatorioController;
+
+    @Autowired
+    RebeldeController rebeldeController;
+
+    @Test
+    public void givenInputShouldGenerateRelatorio() {
+        RebeldeDTO r1=helper("A",false);
+        RebeldeDTO r2=helper("B",true);
+        RebeldeDTO r3=helper("C",false);
+        rebeldeController.createRebelde(r1);
+        rebeldeController.createRebelde(r2);
+        rebeldeController.createRebelde(r3);
+
+        ResponseEntity responseEntity=relatorioController.getReport();
+        assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
+    }
+
+    private RebeldeDTO helper(String nome, boolean traidor) {
+        RebeldeDTO rebeldeDTO = new RebeldeDTO()
+                .setNome(nome)
+                .setTraidor(traidor)
+                .setGenero("M")
+                .setIdade(33)
+                .setLocalizacao(new LocalizacaoDTO().setNome("Galaxy1").setLatitude("1.0").setLongitude("1.0"))
+                .setInventario(new InventarioDTO().setItens(Arrays.asList(new ItemDTO().setNome("Arma").setQuantidade(1).setPontos(4))));
+        return rebeldeDTO;
+    }
+}
